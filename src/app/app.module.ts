3a6bb6caea20abe2c74b1f28f418f25803dbe6da
Elvira Rguez <35432302+ElviraRguez@users.noreply.github.com>
2019-04-13 23:36:00 +0100

    Creación de jugadores implementada. Corrección del listado de jugadores.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   MatButtonModule,
   MatSelectModule,
   MatDatepickerModule,
+  MatNativeDateModule,
   MatCardModule,
   MatFormFieldModule
 } from '@angular/material';
@@ -69,10 +70,13 @@ const appRoutes: Routes = [
     MatButtonModule,
     MatSelectModule,
     MatDatepickerModule,
+    MatNativeDateModule,
     MatCardModule,
     MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/jugador-create/jugador-create.component.ts
+++ src/app/jugador-create/jugador-create.component.ts
@@ -1,43 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
+import { DatePipe } from '@angular/common';
 import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-jugador-create',
   templateUrl: './jugador-create.component.html',
-  styleUrls: ['./jugador-create.component.css']
+  styleUrls: ['./jugador-create.component.css'],
+  providers: [ DatePipe ]
 })
 export class JugadorCreateComponent implements OnInit {
 
   jugadorForm: FormGroup;
-  dni: string = '';
-  nombre:string = '';
-  apellidos:string = '';
-  fechaNacimiento:string = '';
-  pais:string = '';
+  dni: string = null;
+  nombre: string = null;
+  apellidos: string = null;
+  fechaNacimiento: Date = new Date();
+  edad: number = null;
+  pais: string = null;
 
-  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder, private datePipe: DatePipe) { }
 
   ngOnInit() {
     this.jugadorForm = this.formBuilder.group({
-      dni : [null, Validators.required],
+      dni : [null, [Validators.required, Validators.pattern(/^[XYZ]?([0-9]{7,8})([A-Z])$/)]],
       nombre : [null, Validators.required],
       apellidos : [null, Validators.required],
       fechaNacimiento : [null, Validators.required],
-      pais : [null, Validators.required]
+      pais : [null, Validators.required],
+      edad : [null]
     });
   }
 
   onFormSubmit(form: NgForm) {
-    console.log(form);
-    /*
+    form['edad'] = this.calcularEdad(form['fechaNacimiento']);
+
     this.api.postJugador(form)
       .subscribe(res => {
           this.router.navigate(['/jugadores']);
         }, (err) => {
           console.log(err);
         });
-    */
+  }
+
+  private calcularEdad(fecha) {
+    const hoy = new Date();
+    const cumpleanos = new Date(fecha);
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    const m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
   }
 }
